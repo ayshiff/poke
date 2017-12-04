112bf378c7c6e366eba03b749f309482e53c4579
ayshiff <32459935+ayshiff@users.noreply.github.com>
2017-12-04 23:36:24 +0100

    2nd commit

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var Poke = require('./../models/Poke');
 
+var Type = require('./../models/Type');
+
 var router = express.Router();
 
 router.get('/', function(req, res){
@@ -9,6 +11,24 @@ router.get('/', function(req, res){
         res.render('pokes/index.html', {pokemons: pokemons});
     });
 });
+
+router.get('/new', function(req, res){
+    // récupère toutes les données de la base
+    Type.find({}).then(types => {
+            var pokemon = new Poke();
+    res.render('pokes/edit.html', {pokemon: pokemon, types: types});
+    })
+});
+
+router.get('/edit/:id', function(req, res){
+    Type.find({}).then(types => {
+        // Populate nous permet de tranformer un tableau d'object id en tableau avec les types que l'on veut récupérer
+       Poke.findById(req.params.id).then(pokemon => {
+        res.render('pokes/edit.html', {pokemon: pokemon, types: types} )
+    }) 
+    })  
+});
+
 
 router.get('/:id', function(req,res){
     Poke.findById(req.params.id).populate('types').then(function(pokemon){
@@ -18,5 +38,6 @@ router.get('/:id', function(req,res){
         res.status(500).send(err)
     });
 })
+
 
 module.exports = router;
