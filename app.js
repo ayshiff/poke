326569f4f3ebcd39b10253ccd4fa56d242bea9ff
ayshiff <32459935+ayshiff@users.noreply.github.com>
2017-12-05 19:10:23 +0100

    Third part commit

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require ('express');
 var mongoose = require('mongoose');
 var nunjucks = require('nunjucks');
+var bodyParser =require('body-parser');
+var multer = require('multer');
+
+var upload = multer({
+    dest: __dirname + '/uploads'
+});
 
 mongoose.connect('mongodb://localhost/openclassroom');
 
@@ -9,10 +15,15 @@ require('./models/Type');
 
 var app = express();
 
+app.use(bodyParser.urlencoded());
+app.use(upload.single('file'));
+
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 app.use('/', require('./routes/pokemon'));
 app.use('/types', require('./routes/types'));
+
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 nunjucks.configure('views', {
     autoescape: true,
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -16,7 +16,7 @@ router.get('/new', function(req, res){
     // récupère toutes les données de la base
     Type.find({}).then(types => {
             var pokemon = new Poke();
-    res.render('pokes/edit.html', {pokemon: pokemon, types: types});
+    res.render('pokes/edit.html', {pokemon: pokemon, types: types, endpoint: '/'});
     })
 });
 
@@ -24,7 +24,7 @@ router.get('/edit/:id', function(req, res){
     Type.find({}).then(types => {
         // Populate nous permet de tranformer un tableau d'object id en tableau avec les types que l'on veut récupérer
        Poke.findById(req.params.id).then(pokemon => {
-        res.render('pokes/edit.html', {pokemon: pokemon, types: types} )
+        res.render('pokes/edit.html', {pokemon: pokemon, types: types, endpoint: '/' + pokemon.id.toString()} )
     }) 
     })  
 });
@@ -38,6 +38,27 @@ router.get('/:id', function(req,res){
         res.status(500).send(err)
     });
 })
+
+router.post('/:id?', (req, res) => {
+    new Promise((resolve, reject) =>{
+        if(req.params.id){
+            Poke.findById(req.params.id).then(resolve, reject);
+        }else {
+            resolve (new Poke());
+        }
+    }).then(pokemon => {
+        pokemon.name = req.body.name;
+        pokemon.description = req.body.description;
+        pokemon.number = req.body.number;
+        pokemon.types = req.body.types;
+
+        if(req.file) pokemon.picture = request.file.filename;
+
+        return pokemon.save();    
+    }).then(() => {
+        res.redirect('/');
+    }, err => console.log(err))
+});
 
 
 module.exports = router;
